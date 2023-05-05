07ceff4153e98810e851a3801cb1eb76752c92f4
leolanese <laneseleo@gmail.com>
2023-05-05 13:34:15 +0100

    Updating components mate-shop NextJS ReactJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 // default route
 import React from "react";
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
+import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google';
+
+import Head from "next/head";
 import Banner from "../components/banner";
 
 const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 export default function Home() {
   const handleOnBannerBtnClick = () => {
@@ -13,29 +20,32 @@ export default function Home() {
   };
 
   return (
-    <>
-      <main 
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      
-      <Banner buttonText="ClickMe" handleOnClick={handleOnBannerBtnClick} />
+    <main className={inter.className}>
 
+      <Head>
+        <title>The Mate Shop</title>
+        <link rel="icon" href="/favicon.ico"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
 
+      <div 
+        className={`flex min-h-screen flex-col items-center justify-between p-24`} >
+        <Banner buttonText="ClickMe" handleOnClick={handleOnBannerBtnClick} />
+      </div>
 
-    </main>
+      <footer 
+      className={`flex items-center h-full`}>
+        <p className={`pr-15 ${roboto.className}`}>Powered by:</p>
+        <Image
+          className="pl-15 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="NextJS Logo"
+          width={90}
+          height={18}
+          priority
+        />
+      </footer>
 
-    <footer 
-      className={`flex items-center h-full ${inter.className}`}>
-      <Image
-        className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={90}
-        height={18}
-        priority
-      />
-      <p className="pl-15 right">- © 2023 Leo Lanese</p>
-    </footer>
-   </>
-
+   </main>
   )
 }
---
+++ pages/mate-store.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MateStore = () => {
+    return (
+        <div>
+            <p>Mate store here</p>
+        </div>
+    );
+};
+
+export default MateStore;
