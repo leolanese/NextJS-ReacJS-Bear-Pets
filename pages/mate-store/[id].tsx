38a1f566f3ae490984927f2203fa3149f0b3870f
leolanese <laneseleo@gmail.com>
2023-05-05 13:42:43 +0100

    Updating components mate-shop NextJS ReactJS

--- pages/mate-store/[id].tsx
+++ pages/mate-store/[id].tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const MateStore = () => {
+    const router = useRouter()
+    console.log('router: ', router.query.id)
+
     return (
         <div>
             <p>Mate store here</p>
