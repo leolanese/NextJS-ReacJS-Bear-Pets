e3c56ef1bdf605a09fb85dd0e449df690c70f539
leolanese <laneseleo@gmail.com>
2023-05-15 17:26:24 +0100

    Updating index to consume SSG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,87 +1,20 @@
 import React, { useState, useEffect, memo } from 'react'
-import { useRouter } from 'next/router';
+
 import type { AppProps } from 'next/app'
-import UseErrorBoundary from './../components/UseErrorBoundary';
-import CardList from '@/components/CardList';
-import Search from '../components/Search';
 import Image from 'next/image'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [error, throwError] = UseErrorBoundary();
-  const [appState, setAppState] = useState({
-    pets: [], // resultsMock
-    searchField: '',
-  })
-
-  const router = useRouter();
-  const hideFooter = router.pathname === '/bear-pets/[id]';
-
-  const setPets = (pets) => {
-    setAppState((prevState) => ({ ...prevState, pets: pets }));
-    // (prevState => ({ ...prevState, pets: pets })). This ensures that you're always 
-    // using the most recent state when setting the new state
-  };
-  
-  useEffect(() => {
-    const fetchPets = async (signal) => {
-      try {
-        // The AbortController and its signal offer a way to communicate the abort event to the fetch request
-        const response = await fetch('/api/api', { signal });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const pets = await response.json();
-        setPets(pets);
-      } catch (error) {
-        if (error.name === 'AbortError') {
-          console.log('Fetch request was aborted');
-        } else {
-          throwError(error);
-        }
-      }
-    };
-
-    const abortController = new AbortController();
-    fetchPets(abortController.signal);
-
-    return () => {
-      abortController.abort();
-    };
-  }, [throwError]); // if preset, effect will only activate if the values in the list change
-
-  const onSearchChange = (event) => {
-    setAppState(prevState => ({ ...prevState, searchField: event.target.value }));
-  };
-
-  const filteredPets = appState.pets.filter((user) =>
-    user.name.toLowerCase().includes(appState.searchField.toLowerCase())
-  );
-
-  console.table('filteredPets', filteredPets);
   console.log('process.env.LOCAL', process.env.LOCAL);  // http://localhost
   console.log('process.env.PORT', process.env.PORT);   // 3000
   
-  return !appState.pets.length ?
-      <div>
-        <h1>Loading... Please, wait.</h1>
-      </div>
-  : (
+  return (
     <>
-
       <div className={`bg-sky-500/75`}>
 
         <Component {...pageProps} />
-
-        {!hideFooter && (
-          <Search onSearchChange={onSearchChange} />
-        )}
-
-        {!hideFooter && (
-           <CardList pets={filteredPets} />
-        )}
-
+        
         <footer 
             className={`flex flex justify-between h-full p-2`}>
               <Image
